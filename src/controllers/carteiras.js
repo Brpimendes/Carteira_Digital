cb9be22634c4a779380cb15da8e0e3cabbc423b2
Bruno Mendes <brpimendes@gmail.com>
2021-11-17 00:28:36 -0300

    Criacao dos controllers e repositorios para adicionar saldo e realizar compras

--- src/controllers/carteiras.js
+++ src/controllers/carteiras.js
@@ -3,5 +3,23 @@ const walletRepositories = require("../repositories/carteiras");
 exports.getAll = async (req, res) => {
   const carteiras = await walletRepositories.getAll();
 
-  res.send(carteiras);
+  res.json(carteiras);
+};
+
+exports.addSaldo = async (req, res) => {
+  const conta = req.params.conta;
+  const { saldo } = req.body;
+
+  const saldoCarteira = await walletRepositories.addSaldo(conta, saldo);
+
+  res.json(saldoCarteira);
+};
+
+exports.makeShop = async (req, res) => {
+  const conta = req.params.conta;
+  const { shop } = req.body;
+
+  const saldoCarteira = await walletRepositories.makeShop(conta, shop);
+
+  res.json(saldoCarteira);
 };
--- src/repositories/carteiras.js
+++ src/repositories/carteiras.js
@@ -1,3 +1,53 @@
+const { carteira } = require("../models");
+
 exports.getAll = () => {
-  return "Pegando todos os dados das carteiras";
+  return carteira.findAll();
+};
+
+exports.addSaldo = async (conta, valorRecebido) => {
+  const contaExiste = await carteira.findOne({ where: { conta } });
+
+  if (!contaExiste) return "Conta inexistente! Informe a conta correta.";
+
+  if (valorRecebido <= 0)
+    return "O valor a ser depositado precisa ser maior que 0(zero).";
+
+  var saldoUsuario = contaExiste.saldo;
+
+  var saldoAtual = (saldoUsuario += valorRecebido);
+
+  const saldoAtualizado = await carteira.update(
+    {
+      saldo: saldoAtual,
+    },
+    {
+      where: { conta },
+    }
+  );
+
+  return saldoAtualizado;
+};
+
+exports.makeShop = async (conta, shop) => {
+  const contaExiste = await carteira.findOne({ where: { conta } });
+
+  if (!contaExiste)
+    return `Conta inexistente! Favor informar uma conta válida!`;
+
+  var saldoUsuario = contaExiste.saldo;
+
+  if (shop > saldoUsuario) return `Saldo insuficiente para realizar a compra!`;
+
+  var SaldoAtual = (saldoUsuario -= shop);
+
+  const saldoAtualizado = await carteira.update(
+    {
+      saldo: SaldoAtual,
+    },
+    {
+      where: { conta },
+    }
+  );
+
+  return saldoAtualizado;
 };
