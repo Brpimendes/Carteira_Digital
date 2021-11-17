22456ad4e76e2b3b7d4f6e5cd844892f66ea84b5
Bruno Mendes <brpimendes@gmail.com>
2021-11-17 00:26:27 -0300

    Criacao das rotas de adicionar saldo e realizar Compras

--- src/routes/carteiras.js
+++ src/routes/carteiras.js
@@ -4,5 +4,7 @@ const walletController = require("../controllers/carteiras");
 const walletRoutes = express.Router();
 
 walletRoutes.get("/", walletController.getAll);
+walletRoutes.put("/:conta", walletController.addSaldo);
+walletRoutes.put("/fazerCompra/:conta", walletController.makeShop);
 
 module.exports = walletRoutes;
