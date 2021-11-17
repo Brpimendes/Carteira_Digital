190bf6ff631cc8f4c46193cf7c48d6d98c486476
Bruno Mendes <brpimendes@gmail.com>
2021-11-17 00:33:11 -0300

    Adicao do middleware ao servidor e utilzacao do log via console.

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const routes = require("./routes");
+const log = require("./middlewares/log");
 
 require("./database");
 
 const app = express();
 
 app.use(express.json());
+app.use(log);
 app.use(routes);
 
 app.listen(3000, function () {
