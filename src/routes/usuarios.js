66f88fe40227ec7575b573f1810611a8fb6e0991
Bruno Mendes <brpimendes@gmail.com>
2021-11-30 20:53:11 -0300

    Adicao do validator na rota de post ao salvar o usuario.

--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const userController = require("../controllers/usuario");
-
 const userRoutes = express.Router();
+const userValidator = require("../validators/createUsuario");
 
 userRoutes.get("/", userController.getAll);
 userRoutes.get("/:id", userController.getById);
-userRoutes.post("/", userController.salvar);
+userRoutes.post("/", userValidator, userController.salvar);
 userRoutes.put("/:id", userController.atualizar);
 userRoutes.delete("/:id", userController.deletar);
 
