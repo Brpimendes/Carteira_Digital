c9b44df619f9d3f8f96d580e4728dfc6b858b569
Bruno Mendes <brpimendes@gmail.com>
2021-12-02 21:08:26 -0300

    Ajuste na validacao do CPF, acrescentando os campos de min e max, permitindo apenas 11 caracteres.

--- src/validators/createUsuario.js
+++ src/validators/createUsuario.js
@@ -4,7 +4,11 @@ const { create } = require("yup/lib/Reference");
 const createUsuario = async (req, res, next) => {
   const schema = yup.object().shape({
     nome: yup.string().required("O Nome é Obrigatório!"),
-    cpf: yup.string().required("O CPF é Obrigatório"),
+    cpf: yup
+      .string()
+      .required("O CPF é Obrigatório")
+      .min(11, "o CPF deve conter 11 caracteres")
+      .max(11, "o CPF deve conter apenas 11 caracteres"),
     telefone: yup
       .string()
       .max(13, "O telefone não pode ter mais de 15 caracteres."),
