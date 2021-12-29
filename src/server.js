04a90087447774be2e5ab5a2d3901ede88f0129c
Bruno Mendes <brpimendes@gmail.com>
2021-12-29 00:03:06 -0300

    Adicionado o CORS, para permitir troca de informações entre o backend e o frontend de locais diferentes.

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const routes = require("./routes");
 const log = require("./middlewares/log");
+const cors = require("cors");
 
 require("./database");
 
@@ -8,6 +9,7 @@ const app = express();
 
 app.use(express.json());
 app.use(log);
+app.use(cors());
 app.use(routes);
 
 app.listen(3000, function () {
