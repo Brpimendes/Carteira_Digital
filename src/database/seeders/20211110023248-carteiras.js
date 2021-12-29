fb9928216e261064075a34f902b6a47792008571
Bruno Mendes <brpimendes@gmail.com>
2021-12-28 23:55:14 -0300

    Adicao do campo id nos seeders, para que os registros sejam incluidos na extracao dos usuários com as carteiras.

--- src/database/seeders/20211110023248-carteiras.js
+++ src/database/seeders/20211110023248-carteiras.js
@@ -6,16 +6,19 @@ module.exports = {
       "carteira",
       [
         {
+          id: 1,
           usuario_id: 1,
           agencia: "0001",
           saldo: 10.0,
         },
         {
+          id: 2,
           usuario_id: 2,
           agencia: "0001",
           saldo: 20.0,
         },
         {
+          id: 3,
           usuario_id: 3,
           agencia: "0001",
           saldo: 30.0,
